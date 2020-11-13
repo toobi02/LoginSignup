79844ff34b50e72acf0c2e7f6731b779c8ad8035
tooba <toobakashafin11@gmail.com>
2020-11-13 11:52:38 +0530

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginSignup.Models;
+using System.Web.Security;
+
+namespace LoginSignup.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FoodPortal model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                bool isValid = context.User.Any(x=>x.UserName == model.UserName && x.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Customers");
+                }
+
+                ModelState.AddModelError("", "Invalid username and Password");
+            }
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
